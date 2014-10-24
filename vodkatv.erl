8ad18ec908c73b8cca95d3ac6f2eaf7e246cb6b5
miguelafr <miguel.francisco@interoud.com>
2014-10-24 14:20:06 +0200

    Tests for VoDKATV

--- vodkatv.erl
+++ vodkatv.erl
@@ -2,14 +2,17 @@
 
 -compile(export_all).
 
--record(state, {token}).
+-record(channel, {vodkatvChannelId, number, name}).
+-record(state, {token, channels, favorite_channels_ids}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 initial_state() ->
     #state {
-        token = undefined
+        token = undefined,
+        channels = undefined,
+        favorite_channels_ids = []
     }.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -42,34 +45,76 @@ link_permitted(_Super, State, Link) ->
 % next_state
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 next_state(Super, State, Result, Call) ->
+    {_, _, follow_link, [_, {URI, RequestType, Body, QueryParms}], _} = Call, 
     LinkTitle = js_links_machine:call_link_title(Call),
     PrivateState = jsg_links_utils:private_state(State),
     JSONResult = js_links_machine:get_json_body(Result),
 
-    NextPrivateState = next_state_internal(PrivateState, JSONResult, LinkTitle), 
+    NextPrivateState = next_state_internal(PrivateState,
+        {URI,RequestType,Body,QueryParms}, JSONResult, LinkTitle), 
 
     Super(jsg_links_utils:set_private_state(NextPrivateState, State),
 	  Result, Call).
 
-next_state_internal(PrivateState, JSONResult, "login") ->
+next_state_internal(PrivateState, {_URI, _RequestType, _Body, _QueryParms},
+        JSONResult, "login") ->
     Token = jsg_jsonschema:propertyValue(JSONResult, "token"),
     PrivateState#state {
-        token = Token
+        token = erlang:binary_to_list(Token)
     };
 
-next_state_internal(PrivateState, _JSONResult, "logout") ->
+next_state_internal(PrivateState, {_URI, _RequestType, _Body, _QueryParms},
+        JSONResult, "channels") ->
+    Channels = jsg_jsonschema:propertyValue(JSONResult, "channels"),
+    Elements = jsg_jsonschema:propertyValue(Channels, "elements"),
+    PrivateState#state {
+        channels = get_channels(Elements)
+    };
+
+next_state_internal(PrivateState, {_URI, _RequestType, _Body, QueryParms},
+        _JSONResult, "add_to_favorite_channels") ->
+
+    QueryParamVodkatvChannelId = proplists:get_value("vodkatvChannelId",
+        QueryParms),
+
+    case lists:member(QueryParamVodkatvChannelId,
+            PrivateState#state.favorite_channels_ids) of
+        false ->
+            PrivateState#state {
+                favorite_channels_ids = [QueryParamVodkatvChannelId |
+                    PrivateState#state.favorite_channels_ids]
+            };
+        true ->
+            PrivateState
+    end;
+
+next_state_internal(PrivateState, {_URI, _RequestType, _Body, QueryParms},
+        _JSONResult, "remove_favorite_channel") ->
+
+    QueryParamVodkatvChannelId = proplists:get_value("vodkatvChannelId",
+        QueryParms),
+
+    PrivateState#state {
+        favorite_channels_ids = lists:delete(QueryParamVodkatvChannelId,
+                PrivateState#state.favorite_channels_ids)
+    };
+
+
+next_state_internal(PrivateState, {_URI, _RequestType, _Body, _QueryParms},
+        _JSONResult, "logout") ->
     PrivateState#state {
         token = undefined
     };
 
-next_state_internal(PrivateState, _JSONResult, _LinkTitle) ->
+next_state_internal(PrivateState, {_URI, _RequestType, _Body, _QueryParms},
+        _JSONResult, _LinkTitle) ->
     PrivateState.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % postcondition
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 postcondition(Super, State, Call, Result) ->
-    {_, _, follow_link, [_,{URI,RequestType,Body,QueryParms}], _} = Call, 
+    {_, _, follow_link, [_, {URI, RequestType, Body, QueryParms}], _} = Call, 
     LinkTitle = js_links_machine:call_link_title(Call),
     PrivateState = jsg_links_utils:private_state(State),
     case js_links_machine:validate_call_not_error_result(Call,Result) of
@@ -84,11 +129,33 @@ postcondition(Super, State, Call, Result) ->
       false -> false
     end.
 
-postcondition_internal(_PrivateState, {_URI, _RequestType, _Body, _QueryParms},
+postcondition_internal(PrivateState, {_URI, _RequestType, _Body, _QueryParms},
         JSONResult, "login") ->
-    Token = jsg_jsonschema:propertyValue(JSONResult, "token"),
     ValidUntil = jsg_jsonschema:propertyValue(JSONResult, "validUntil"),
-    Token =/= "" andalso ValidUntil > get_now();
+    PrivateState#state.token =/= "" andalso ValidUntil > get_now();
+
+postcondition_internal(PrivateState, {_URI, _RequestType, _Body, QueryParms},
+        JSONResult, "channel") ->
+    
+    % Get channels from the state
+    case PrivateState#state.channels of
+        undefined ->
+            % We can not check anything here if we don't have channels
+            true;
+        Channels ->
+            % Get params
+            QueryParamVodkatvChannelId = proplists:get_value("vodkatvChannelId", QueryParms),
+
+            % Search channel in the state and compare
+            case [Channel || Channel <- Channels,
+                    Channel#channel.vodkatvChannelId == QueryParamVodkatvChannelId] of
+                [Channel] ->
+                    Channel == get_channel(
+                        jsg_jsonschema:propertyValue(JSONResult, "channel"));
+                _ ->
+                    false
+            end
+    end;
 
 postcondition_internal(_PrivateState, {_URI, _RequestType, _Body, _QueryParms},
         JSONResult, "languages") ->
@@ -108,6 +175,22 @@ postcondition_internal(_PrivateState, {_URI, _RequestType, _Body, _QueryParms},
     Elements = jsg_jsonschema:propertyValue(Categories, "elements"),
     erlang:length(Elements) >= 0;
 
+postcondition_internal(PrivateState, {_URI, _RequestType, _Body, _QueryParms},
+        JSONResult, LinkTitle) when LinkTitle =:= "favorite_channels";
+                                    LinkTitle =:= "add_to_favorite_channels";
+                                    LinkTitle =:= "remove_favorite_channel" ->
+    Channels = jsg_jsonschema:propertyValue(JSONResult, "channels"),
+    Elements = jsg_jsonschema:propertyValue(Channels, "elements"),
+    FavoriteChannelIds = PrivateState#state.favorite_channels_ids,
+
+    FavoriteChannelIdsFromState = lists:map(fun(Element) ->
+        Channel = get_channel(Element),
+        Channel#channel.vodkatvChannelId
+    end, Elements),
+    lists:all(fun(FavoriteChannelId) ->
+        lists:member(FavoriteChannelId, FavoriteChannelIdsFromState)
+    end, FavoriteChannelIds);
+
 postcondition_internal(_PrivateState, {_URI, _RequestType, _Body, _QueryParms},
         JSONResult, "logout") ->
     JSONResult == {struct,[]};
@@ -118,6 +201,21 @@ postcondition_internal(_PrivateState, _Call, _JSONResult, _LinkTitle) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Utilities
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+get_channel(Element) ->
+    #channel {
+        vodkatvChannelId = erlang:binary_to_list(
+            jsg_jsonschema:propertyValue(Element, "vodkatvChannelId")),
+        number = jsg_jsonschema:propertyValue(Element, "number"),
+        name = erlang:binary_to_list(
+            jsg_jsonschema:propertyValue(Element, "name"))
+    }.
+
+get_channels([]) ->
+    [];
+get_channels([Element | Elements]) ->
+    Channel = get_channel(Element),
+    [Channel | get_channels(Elements)].
+
 get_now() ->
     {MegaSecs, Secs, _MicroSecs} = erlang:now(),
     (MegaSecs * 1000000 + Secs)*1000.
