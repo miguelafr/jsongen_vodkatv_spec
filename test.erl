2a065d06bf1353a6f1c7646546b520398f7a914d
miguelafr <miguel.francisco@interoud.com>
2014-12-02 09:04:54 +0100

    jsongen specification

--- test.erl
+++ test.erl
@@ -4,7 +4,7 @@
 test1() ->
   js_links_machine:run_statem(vodkatv,["login.jsch","login_response.jsch",
         "channels_response.jsch", "channel.jsch"],
-        [{show_uri,true}, {validator,jesse_validator}]).
+        [{timeout,5000}, {show_uri,true}, {validator,jesse_validator}]).
 
 test1(N) ->
   js_links_machine:run_statem(vodkatv,["login.jsch","login_response.jsch",
